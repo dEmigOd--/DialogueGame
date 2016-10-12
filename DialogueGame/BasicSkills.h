97d5af9f5cf8d15369d92b4a2b592c2013b0ae12
dEmigOd-- <Hardly.Enticed@gmail.com>
2016-10-12 11:39:00 +0300

    splitting skills into basic and regular

---
+++ DialogueGame/BasicSkills.h
@@ -0,0 +1,154 @@
+#pragma once
+
+#include <string>
+#include <map>
+#include "ReadWriteToFile.h"
+
+enum BasicSkill : int
+{
+	Accuracy,
+	SkillBegin = Accuracy,
+	Dexterity,
+	Speed,
+	Stamina,
+	Strength,
+	SkillEnd,
+};
+
+std::string ToString(BasicSkill skill)
+{
+	switch (skill)
+	{
+	case Accuracy:
+		return "Accuracy";
+	case Dexterity:
+		return "Dexterity";
+	case Speed:
+		return "Speed";
+	case Stamina:
+		return "Stamina";
+	case Strength:
+		return "Strength";
+	default:
+		return "Unknown";
+	}
+}
+
+std::ostream& operator<<(std::ostream& os, BasicSkill skill)
+{
+	return os << ToString(skill);
+}
+
+
+
+namespace std
+{
+	template<>
+	struct iterator_traits<BasicSkill>
+	{
+		typedef BasicSkill  value_type;
+		typedef int    difference_type;
+		typedef BasicSkill *pointer;
+		typedef BasicSkill &reference;
+		typedef std::bidirectional_iterator_tag
+			iterator_category;
+	};
+}
+
+BasicSkill &operator++(BasicSkill &c)
+{
+	c = static_cast<BasicSkill>(c + 1);
+	return c;
+}
+
+BasicSkill operator++(BasicSkill &c, int)
+{
+	++c;
+	return static_cast<BasicSkill>(c - 1);
+}
+
+BasicSkill &operator--(BasicSkill &c)
+{
+	return c = static_cast<BasicSkill>(c - 1);
+}
+
+BasicSkill operator--(BasicSkill &c, int)
+{
+	--c;
+	return static_cast<BasicSkill>(c + 1);
+}
+
+BasicSkill operator*(BasicSkill c)
+{
+	return c;
+}
+
+std::map<BasicSkill, int> EmptySkillsBag()
+{
+	std::map<BasicSkill, int> bag;
+
+	for (BasicSkill skill = BasicSkill::SkillBegin; skill < BasicSkill::SkillEnd; ++skill)
+	{
+		bag[skill] = 0;
+	}
+
+	return bag;
+}
+
+class BasicSkillVectorReaderWriter : ReadWriteToFile
+{
+	std::map<BasicSkill, int> skills;
+public:
+
+	BasicSkillVectorReaderWriter()
+	{
+	}
+
+	BasicSkillVectorReaderWriter(const std::map<BasicSkill, int>& skills)
+		: skills(skills)
+	{
+	}
+
+	virtual std::ostream& PrintToStream(std::ostream& os) const
+	{
+		for (auto const& skill : skills)
+		{
+			os << static_cast<int>(skill.first) << " " << skill.second << std::endl;
+		}
+		return os;
+	}
+
+	virtual std::istream& ReadFromStream(std::istream& is)
+	{
+		std::map<BasicSkill, int> readSkills;
+
+		int bskill, value;
+
+		for (BasicSkill skill = BasicSkill::SkillBegin; skill < BasicSkill::SkillEnd; ++skill)
+		{
+			is >> bskill >> value;
+
+			readSkills.insert(std::make_pair(static_cast<BasicSkill>(skill), value));
+		}
+
+		skills.swap(readSkills);
+
+		return is;
+	}
+
+	std::map<BasicSkill, int> Skills() const
+	{
+		return skills;
+	}
+};
+
+std::ostream& operator<<(std::ostream& os, const BasicSkillVectorReaderWriter& rw)
+{
+	return rw.PrintToStream(os);
+}
+
+std::istream& operator >> (std::istream& is, BasicSkillVectorReaderWriter& rw)
+{
+	return rw.ReadFromStream(is);
+}
+
