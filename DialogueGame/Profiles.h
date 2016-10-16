c37613ec3792f59c3213be6ebc37a52ccf0a048f
dEmigOd-- <Hardly.Enticed@gmail.com>
2016-10-16 10:58:18 +0300

    add more profiles

--- DialogueGame/Profiles.h
+++ DialogueGame/Profiles.h
@@ -9,4 +9,31 @@ public:
 	{
 		return CharacterProfile();
 	}
+
+	static CharacterProfile WarriorProfile()
+	{
+		CharacterProfile profile;
+
+		profile[Durability]	+= 1;
+		profile[Stamina]	+= 1;
+		profile[Strength]	+= 2;
+	}
+
+	static CharacterProfile RogueProfile()
+	{
+		CharacterProfile profile;
+
+		profile[Agility] += 1;
+		profile[Dexterity] += 1;
+		profile[Reflexes] += 1;
+		profile[Speed] += 1;
+	}
+
+	static CharacterProfile WizardProfile()
+	{
+		CharacterProfile profile;
+
+		profile[Intelligence] += 3;
+		profile[Reflexes] += 1;
+	}
 };
